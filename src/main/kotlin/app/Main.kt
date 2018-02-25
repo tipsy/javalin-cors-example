fd87d337f6f3523db9ea12d385376d7502cc1d39
David <davidaase@hotmail.com>
2018-02-25 14:48:37 +0100

    init example

---
+++ src/main/kotlin/app/Main.kt
@@ -0,0 +1,27 @@
+package app
+
+import io.javalin.Javalin
+import org.json.JSONObject
+import java.util.*
+
+fun main(args: Array<String>) {
+
+    val corsApp = Javalin.create().apply {
+        port(80)
+        enableCorsForAllOrigins()
+        enableStandardRequestLogging()
+        get("/") { ctx ->
+            val res = JSONObject()
+            res.put("version", "1.0.0")
+            res.put("date", Date().toString())
+            res.put("contact", "admin@domain.com")
+            ctx.result(res.toString()).contentType("application/json")
+        }
+    }.start()
+
+    val frontendApp = Javalin.create().apply {
+        port(8081)
+        enableStaticFiles("/public")
+    }.start()
+
+}
